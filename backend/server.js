f3162ab409a29ba85d2e667adddd46030bff0e40
Tanjim605 <tanjimulislam2001@gmail.com>
2024-05-14 13:12:18 +0600

    mongodb database is connected with the server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
 const port = process.env.PORT || 8593           // port defined as localhost:8593 or hosting port
 
-const express = require('express')      
+const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 
+const teacherRoutes = require('./routes/teacher-route')
+
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('../frontend/public'))
+
+
+mongoose.connect('mongodb+srv://c213076:qZPntQBkLAauWGw1@backenddb.pwut9sa.mongodb.net/the_boring_quiz_DB?retryWrites=true&w=majority&appName=BackendDB')
+    .then(() => {
+        console.log(`connected to the the_boring_quiz_DB`)
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
+        })
+    })
+    .catch(err=>{
+        console.log(`MongoDB connection FAILED ${err}`)
+    })
